ffa57fcb5da5fc1c46cf6bc7293eb41c416b6f70
John Rihacek <jrihacek@gmail.com>
2019-05-03 20:25:21 -0400

    start routes, merge web template with Uptime

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,11 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "hello world!"
+    return render_template('index.html')
+    
+@app.route('/hello/<user>')
+def hello_name(user):
+    return render_template('hello.html', name = user)
